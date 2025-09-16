931c8c9e3abb17ca34573044dfb1009b08941681
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-09-16 21:35:31 +0530

    started with builder pattern

---
+++ src/main/java/com/design/patterns/builder/Builder.java
@@ -0,0 +1,11 @@
+package com.design.patterns.builder;
+
+public interface Builder {
+
+	void buildBody();
+	void addWheels();
+	void addHeadlights();
+	void startOperations();
+	void endOperations();
+	
+}
