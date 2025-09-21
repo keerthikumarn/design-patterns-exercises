8a785370dec7d4de1b750ce9f13bd13bd2450009
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-09-21 16:21:07 +0530

    Updated the factory class

--- src/main/java/com/design/patterns/factory/AnimalFactory.java
+++ src/main/java/com/design/patterns/factory/AnimalFactory.java
@@ -1,6 +1,14 @@
 package com.design.patterns.factory;
 
 public abstract class AnimalFactory {
+	
+	public Animal makeAnimal() {
+		System.out.println("I am inside makeAnimal() of AnimalFactory.You cannot ignore my rules.");
+		Animal animal = createAnimal();
+		animal.makeSound();
+		animal.performAction();
+		return animal;
+	}
 
 	public abstract Animal createAnimal();
 }
