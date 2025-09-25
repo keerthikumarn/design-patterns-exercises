54433f567228b875c90d528a102e19e232908bf7
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-09-25 14:58:19 +0530

    included demo class

---
+++ src/main/java/com/design/patterns/proxy/ProxyPatternDemo.java
@@ -0,0 +1,11 @@
+package com.design.patterns.proxy;
+
+public class ProxyPatternDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Proxy pattern demo");
+		Proxy proxy = new Proxy();
+		proxy.performSomeAction();
+	}
+
+}
