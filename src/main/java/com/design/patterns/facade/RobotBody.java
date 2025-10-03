1632774b448d682db6229955ed4ed850be36e8e4
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-03 23:22:09 +0530

    started with facade pattern

---
+++ src/main/java/com/design/patterns/facade/RobotBody.java
@@ -0,0 +1,28 @@
+package com.design.patterns.facade;
+
+public class RobotBody {
+
+	public static void createRobot() {
+		System.out.println("Refer this manual before creating a Robot");
+	}
+
+	public void createHands() {
+		System.out.println("Robot Hands Created");
+	}
+
+	public void createRemainingParts() {
+		System.out.println("Remaining parts (other than hands) are\n" + "created.");
+	}
+
+	public static void destroyRobot() {
+		System.out.println(" Refer the manual before destroying of a robot.");
+	}
+
+	public void destroyHands() {
+		System.out.println(" The robot's hands are destroyed.");
+	}
+	
+	public void destroyRemainingParts(){
+		System.out.println(" The robot's remaining parts are destroyed.");
+	}
+}
