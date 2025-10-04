3acfb987f0e8086d17d9a49df2be0e9b8d1d7341
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-04 19:54:10 +0530

    started with facade pattern

---
+++ src/main/java/com/design/patterns/facade/RobotFacade.java
@@ -0,0 +1,37 @@
+package com.design.patterns.facade;
+
+public class RobotFacade {
+
+	RobotColor robotColor;
+	RobotHands robotHands;
+	RobotBody robotBody;
+
+	public RobotFacade() {
+		robotColor = new RobotColor();
+		robotHands = new RobotHands();
+		robotBody = new RobotBody();
+	}
+
+	public void constructMilanoRobot() {
+		RobotBody.createRobot();
+		System.out.println("Creation of a Milano Robot Start.");
+		robotColor.setDefaultColor();
+		robotHands.setMilanoHands();
+		robotBody.createHands();
+		robotBody.createRemainingParts();
+		System.out.println(" Milano Robot Creation End.");
+		System.out.println();
+	}
+
+	public void constructRobonautRobot() {
+		RobotBody.createRobot();
+		System.out.println("Initiating the creational process of a Robonaut Robot.");
+		robotColor.setColor("Green");
+		robotHands.setRobonautHands();
+		robotBody.createHands();
+		robotBody.createRemainingParts();
+		System.out.println("A Robonaut Robot is created.");
+		System.out.println();
+	}
+
+}
