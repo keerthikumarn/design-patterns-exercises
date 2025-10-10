bdcac2accee0c0f9b9caecdcb427c4d43c6c8e10
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-10 21:21:09 +0530

    impl goal notifier

--- src/main/java/com/design/patterns/observer/GoalNotifier.java
+++ src/main/java/com/design/patterns/observer/GoalNotifier.java
@@ -2,4 +2,18 @@ package com.design.patterns.observer;
 
 public class GoalNotifier implements FitnessDataObserver {
 
+	private final int stepGoal = 10000;
+	private boolean goalReached = false;
+	
+	@Override
+	public void update(FitnessData data) {
+		if (data.getSteps() >= stepGoal && !goalReached) {
+			goalReached = true;
+			System.out
+					.println("GoalNotifier: Congratulations! You've reached your step goal of " + stepGoal + " steps!");
+		} else if (data.getSteps() < stepGoal && goalReached) {
+			goalReached = false;
+		}
+	}
+
 }
