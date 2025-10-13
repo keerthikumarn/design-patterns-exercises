b33fdb763d158690ef6a7da627898407fd6c8d29
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-13 17:18:41 +0530

    implemented naive class for progress logger

---
+++ src/main/java/com/design/patterns/observer/ProgressLoggerNaive.java
@@ -0,0 +1,8 @@
+package com.design.patterns.observer;
+
+public class ProgressLoggerNaive {
+	public void logDataPoint(int steps, int activeMinutes, int calories) {
+		System.out.println("NAIVE Logger: Saving data - Steps: " + steps + ", Active Mins: " + activeMinutes
+				+ ", Calories: " + calories);
+	}
+}
