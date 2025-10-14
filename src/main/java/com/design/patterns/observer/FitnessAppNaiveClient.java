2e1201f5ca9ede3fd76b8b470ad403f226a90dde
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-14 21:22:21 +0530

    client class

---
+++ src/main/java/com/design/patterns/observer/FitnessAppNaiveClient.java
@@ -0,0 +1,16 @@
+package com.design.patterns.observer;
+
+public class FitnessAppNaiveClient {
+
+	public static void main(String[] args) {
+		LiveActivityDisplayNaive display = new LiveActivityDisplayNaive();
+		ProgressLoggerNaive logger = new ProgressLoggerNaive();
+		NotificationServiceNaive notifier = new NotificationServiceNaive();
+		FitnessDataNaive fitnessData = new FitnessDataNaive(display, logger, notifier);
+		fitnessData.newFitnessDataPushed(500, 5, 20);
+		fitnessData.newFitnessDataPushed(9800, 85, 350);
+		fitnessData.newFitnessDataPushed(10100, 90, 380);
+		fitnessData.dailyReset();
+	}
+
+}
