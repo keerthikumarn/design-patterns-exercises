1c24048efe2aa926e58fb8a8685874835e5445d2
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-15 22:33:07 +0530

    started with state pattern

---
+++ src/main/java/com/design/patterns/state/MachineState.java
@@ -0,0 +1,11 @@
+package com.design.patterns.state;
+
+public interface MachineState {
+
+	void selectItem(VendingMachine context, String itemCode);
+
+	void insertCoin(VendingMachine context, double amount);
+
+	void dispenseItem(VendingMachine context);
+
+}
---
+++ src/main/java/com/design/patterns/state/VendingMachine.java
@@ -0,0 +1,5 @@
+package com.design.patterns.state;
+
+public class VendingMachine {
+
+}
